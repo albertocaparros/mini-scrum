8376cf3e2b9abfe7e7a03d76545b7e3dfff692ea
Alberto Caparrós Trigo <Alberto.Caparros.Trigo@unit4.com>
2025-06-15 20:18:34 +0200

    Clean placeholder content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('mini-scrum');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mini-scrum');
-  });
 });
